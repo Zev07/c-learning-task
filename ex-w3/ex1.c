09e49deb295dde17c3ad68951af89fc424a7d010
Zev <90863634+Zev07@user.noreply.github.com>
2024-11-19 20:13:06 -0300

    update

---
+++ ex-w3/ex1.c
@@ -0,0 +1,12 @@
+// Write a C program to print your name, date of birth, and mobile number.
+/* Expected Output:Name   : Alexandra Abramov  
+DOB    : July 14, 1975  
+Mobile : 99-9999999999 */
+#include <stdio.h>
+
+int main() {
+
+    printf("Name   : Alexandra Abramov\n DOB   : July 14, 1975 \n Mobile: 99-9999999999");
+
+    return 0;
+}
