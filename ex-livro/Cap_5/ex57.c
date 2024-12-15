ac31115d93e90f29c608ce2fbcb33d0f4984ca35
Zev07 <jose.victor.zev7dev@gmail.com>
2024-12-14 21:51:59 -0300

    update

--- ex-livro/Cap_5/ex57.c
+++ ex-livro/Cap_5/ex57.c
@@ -1,1 +1,20 @@
 /*Elabore um programa que peça ao usuário para digitar 10 valores. Some esses valores e apresente o resultado na tela.*/
+
+#include <stdio.h>
+
+int main() {
+    int soma = 0;
+    int valor;
+    int i;
+    const char *sufixos[] = {"primeiro", "segundo", "terceiro", "quarto", "quinto", "sexto", "setimo", "oitavo", "nono", "decimo"};
+
+    for (i = 0; i < 10; i++) {
+        printf("Digite o %s valor: ", sufixos[i]);
+        scanf("%d", &valor);
+        soma += valor;
+    }
+
+    printf("A soma dos valores eh: %d\n", soma);
+
+    return 0;
+}
