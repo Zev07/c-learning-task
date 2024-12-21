e9e3f3b750314efe0ef425ab4174de9106bd3c3c
Zev07 <jose.victor.zev7dev@gmail.com>
2024-12-21 15:32:18 -0300

    update

--- ex-livro/Cap_5/ex68.c
+++ ex-livro/Cap_5/ex68.c
@@ -1,1 +1,25 @@
-/*Faça um programa que receba um número inteiro maior do que 1 e verifique se o número fornecido é primo ou não.*/
+#include <stdio.h>
+#include <math.h>
+
+int main () {
+    int numero;
+
+    do {
+        printf("Digite um numero inteiro positivo maior que 1: ");
+        scanf("%d", &numero);
+
+        if (numero <= 1) {
+            printf("O numero fornecido eh invalido. Tente novamente.\n");
+        }
+    } while (numero <= 1);
+
+    for (int i = 2; i <= sqrt(numero); i++) {
+        if (numero % i == 0) {
+            printf("O numero nao eh primo\n");
+            return 0;
+        }
+    }
+
+    printf("O numero eh primo\n");
+    return 0;
+}
